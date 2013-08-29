fbd37951b26ee3c2b6128c88765ab423bd152d17
Fedor Korotkiy <prime@yandex-team.ru>
2013-08-29 14:15:46 +0400

    Update to new phantom

--- phantom/io_benchmark/method_datagram/method_datagram.C
+++ phantom/io_benchmark/method_datagram/method_datagram.C
@@ -15,6 +15,46 @@ namespace phantom { namespace io_benchmark {
 
 MODULE(io_benchmark_method_datagram);
 
+struct method_datagram_t::loggers_t : sarray1_t<logger_t *> {
+	inline loggers_t(
+		config::list_t<config::objptr_t<logger_t>> const &list
+	) :
+		sarray1_t<logger_t *>(list) { }
+
+	inline ~loggers_t() throw() { }
+
+	inline void init(string_t const &name) const {
+		for(size_t i = 0; i < size; ++i)
+			items[i]->init(name);
+	}
+
+	inline void run(string_t const &name) const {
+		for(size_t i = 0; i < size; ++i)
+			items[i]->run(name);
+	}
+
+	inline void stat_print(string_t const &name) const {
+		stat::ctx_t ctx(CSTR("loggers"), 1);
+		for(size_t i = 0; i < size; ++i)
+			items[i]->stat_print(name);
+	}
+
+	inline void fini(string_t const &name) const {
+		for(size_t i = 0; i < size; ++i)
+			items[i]->fini(name);
+	}
+
+	inline void commit(
+		in_segment_t const &request, in_segment_t &tag, result_t const &res
+	) const {
+		for(size_t i = 0; i < size; ++i) {
+			logger_t *logger = items[i];
+			if(res.log_level >= logger->level)
+				logger->commit(request, tag, res);
+		}
+	}
+};
+
 method_datagram_t::config_t::config_t() {}
 
 void method_datagram_t::config_t::check(const in_t::ptr_t& ptr) const {
@@ -23,40 +63,21 @@ void method_datagram_t::config_t::check(const in_t::ptr_t& ptr) const {
     }
 }
 
-void method_datagram_t::loggers_t::commit(const in_segment_t& request,
-                                          in_segment_t& tag,
-                                          const result_t& res) const {
-    for(size_t i = 0; i < size; ++i) {
-        logger_t *logger = items[i];
-        if(res.log_level >= logger->level)
-            logger->commit(request, tag, res);
-    }
-}
+method_datagram_t::method_datagram_t(const string_t& name, const config_t& config)
+    : method_t(name), source(config.source), loggers(new loggers_t(config.loggers)), datagram_fd(-1), timeout(config.timeout) {}
 
-method_datagram_t::method_datagram_t(const string_t&, const config_t& config)
-    : source(config.source), datagram_fd(-1), timeout(config.timeout) {
+method_datagram_t::~method_datagram_t() {}
 
-    for(typeof(config.loggers.ptr()) lptr = config.loggers; lptr; ++lptr)
-        ++loggers.size;
-
-    loggers.items = new logger_t *[loggers.size];
-
-    size_t i = 0;
-    for(typeof(config.loggers.ptr()) lptr = config.loggers; lptr; ++lptr)
-        loggers.items[i++] = lptr.val();
-}
-
-bool method_datagram_t::test(stat_t& stat) const {
+bool method_datagram_t::test(times_t& times) const {
     in_segment_t request, tag;
     if(!source->get_request(request, tag)) {
         return false;
     }
 
-    stat_t::tcount_guard_t tcount_guard(stat);
     result_t res;
 
     res.size_out = request.size();
-    res.time_conn = timeval_current();
+    res.time_conn = timeval::current();
 
     interval_t _timeout = timeout;
 
@@ -90,31 +111,32 @@ bool method_datagram_t::test(stat_t& stat) const {
         res.log_level = logger_t::network_error;
     }
 
-    res.time_send = res.time_recv = res.time_end = timeval_current();
+    res.time_send = res.time_recv = res.time_end = timeval::current();
     res.interval_event = res.time_end - res.time_start;
 
-    {
-        thr::spinlock_guard_t guard(stat.spinlock);
+    interval_t interval_real = res.time_end - res.time_start;
+    times.inc(interval_real);
 
-        stat.update_time(res.time_end - res.time_start, res.interval_event);
-
-        stat.event();
-
-        stat.update(0, res.err);
-        if(!res.err) {
-            stat.update_size(res.size_in, res.size_out);
-        }
-    }
-
-    loggers.commit(request, tag, res);
+    loggers->commit(request, tag, res);
 
     return true;
 }
 
-void method_datagram_t::init() {
-    source->init();
+void method_datagram_t::do_init() {
+    source->init(name);
+    loggers->init(name);
 
     datagram_fd = connect_fd();
+}
+
+void method_datagram_t::do_run() const {
+    source->run(name);
+    loggers->run(name);
+}
+
+void method_datagram_t::do_stat_print() const {
+    source->stat_print(name);
+    loggers->stat_print(name);
 }
 
 int method_datagram_t::connect_fd() {
@@ -134,52 +156,15 @@ int method_datagram_t::connect_fd() {
     return fd;
 }
 
-void method_datagram_t::stat(out_t& out, bool clear, bool hrr_flag) const {
-    source->stat(out, clear, hrr_flag);
-}
-
-void method_datagram_t::fini() {
-    source->fini();
+void method_datagram_t::do_fini() {
+    source->fini(name);
+    loggers->fini(name);
 
     if(::close(datagram_fd) < 0) {
         log_error("close: %m");
     }
 
     datagram_fd = -1;
-}
-
-size_t method_datagram_t::maxi() const throw() {
-    return 1;
-}
-
-class network_descr_t : public descr_t {
-    static size_t const max_errno = 140;
-
-    virtual size_t value_max() const { return max_errno; }
-
-    virtual void print_header(out_t &out) const {
-        out(CSTR("network"));
-    }
-
-    virtual void print_value(out_t &out, size_t value) const {
-        if(value < max_errno) {
-            char buf[128];
-            char *res = strerror_r(value, buf, sizeof(buf));
-            buf[sizeof(buf) - 1] = '\0';
-            out(str_t(res, strlen(res)));
-        }
-        else
-            out(CSTR("Unknown error"));
-    }
-public:
-    inline network_descr_t() throw() : descr_t() { }
-    inline ~network_descr_t() throw() { }
-};
-
-static network_descr_t const network_descr;
-
-descr_t const* method_datagram_t::descr(size_t /*ind*/) const throw() {
-    return &network_descr;
 }
 
 class method_datagram_ipv4_t : public method_datagram_t {
@@ -255,13 +240,14 @@ config_binding_value(method_datagram_t, source);
 config_binding_type(method_datagram_t, logger_t);
 config_binding_value(method_datagram_t, loggers);
 config_binding_value(method_datagram_t, timeout);
+config_binding_cast(method_datagram_t, method_t);
 } // namespace method_datagram
 
 namespace method_datagram_ipv4 {
 config_binding_sname(method_datagram_ipv4_t);
 config_binding_value(method_datagram_ipv4_t, address);
 config_binding_value(method_datagram_ipv4_t, port);
-config_binding_parent(method_datagram_ipv4_t, method_datagram_t, 1);
+config_binding_parent(method_datagram_ipv4_t, method_datagram_t);
 config_binding_ctor(method_t, method_datagram_ipv4_t);
 } // namespace method_datagram_ipv4
 
@@ -269,7 +255,7 @@ namespace method_datagram_ipv6 {
 config_binding_sname(method_datagram_ipv6_t);
 config_binding_value(method_datagram_ipv6_t, address);
 config_binding_value(method_datagram_ipv6_t, port);
-config_binding_parent(method_datagram_ipv6_t, method_datagram_t, 1);
+config_binding_parent(method_datagram_ipv6_t, method_datagram_t);
 config_binding_ctor(method_t, method_datagram_ipv6_t);
 } // namespace method_datagram_ipv6
 
